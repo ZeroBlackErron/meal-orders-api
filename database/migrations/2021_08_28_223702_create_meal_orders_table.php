f0071bc0b11453855248659da159988e075b3326
Anthony Rosado <arosado-26@hotmail.com>
2021-08-29 04:48:33 -0500

    Modify migration for 'meal_orders' table
    Change table name from 'meal_order' to 'meal_orders'

--- database/migrations/2021_08_28_223702_create_meal_orders_table.php
+++ database/migrations/2021_08_28_223702_create_meal_orders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMealOrderTable extends Migration
+class CreateMealOrdersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateMealOrderTable extends Migration
      */
     public function up()
     {
-        Schema::create('meal_order', function (Blueprint $table) {
+        Schema::create('meal_orders', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('description');
@@ -31,6 +31,6 @@ class CreateMealOrderTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('meal_order');
+        Schema::dropIfExists('meal_orders');
     }
 }
