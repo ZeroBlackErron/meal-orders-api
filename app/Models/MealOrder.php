f8cf3146f5113d4787e144bdf652b0cff0453cdc
Anthony Rosado <arosado-26@hotmail.com>
2021-08-29 01:20:44 -0500

    Create meal order model
    Add migration to create 'meal_order' table

---
+++ app/Models/MealOrder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MealOrder extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function meal()
+    {
+        return $this->belongsTo(Meal::class);
+    }
+}
---
+++ database/migrations/2021_08_28_223702_create_meal_order_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMealOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meal_order', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('price');
+            $table->foreignId('meal_id')->constrained();
+            $table->foreignId('order_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meal_order');
+    }
+}
