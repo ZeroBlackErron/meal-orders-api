bf31413df60bdfaef19a84e3fb930bb461a8fb0e
Anthony Rosado <arosado-26@hotmail.com>
2021-08-29 01:18:06 -0500

    Create meal model and factory
    Add migration to create 'meals' table

---
+++ database/factories/MealFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Meal;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class MealFactory extends Factory
+{
+    protected $model = Meal::class;
+
+    public function definition()
+    {
+        return [
+            'name' => Str::ucfirst((string)$this->faker->words(2, true)),
+            'description' => $this->faker->text(),
+            'price' => $this->faker->randomFloat(2, 0, 50),
+        ];
+    }
+}
