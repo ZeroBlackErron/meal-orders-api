553451b8984a46d491f42248481a5ec18e61d338
Anthony Rosado <arosado-26@hotmail.com>
2021-08-30 19:34:19 -0500

    Use service on login
    Generate token for the user in LoginController using AuthService

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,16 +5,24 @@ namespace App\Http\Controllers;
 use App\Http\Requests\LoginRequest;
 use App\Http\Resources\UserTokenResource;
 use App\Models\User;
+use App\Services\AuthService;
 use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
+    private $service;
+
+    public function __construct(AuthService $authService)
+    {
+        $this->service = $authService;
+    }
+
     public function __invoke(LoginRequest $request)
     {
         if (Auth::attempt($request->validated())) {
             /** @var User $user */
             $user = Auth::user();
-            $token = $user->createToken('login')->plainTextToken;
+            $token = $this->service->generateToken($user);
 
             return UserTokenResource::make($user, $token);
         }
